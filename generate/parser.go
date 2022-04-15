10c670eac27af29a5f078431a06f2ed90ea69dd5
soasurs <soasurs@gmail.com>
2022-04-15 12:39:39 +0800

    fix: remove duplicated path params in request body
    Signed-off-by: soasurs <soasurs@gmail.com>

--- generate/parser.go
+++ generate/parser.go
@@ -263,6 +263,10 @@ func renderReplyAsDefinition(d swaggerDefinitionsObject, m messageMap, p []spec.
 		schema.Title = defineStruct.Name()
 
 		for _, member := range defineStruct.Members {
+			if hasPathParameters(member) {
+				continue
+			}
+
 			kv := keyVal{Value: schemaOfField(member)}
 			kv.Key = member.Name
 			if tag, err := member.GetPropertyName(); err == nil {
@@ -297,6 +301,15 @@ func renderReplyAsDefinition(d swaggerDefinitionsObject, m messageMap, p []spec.
 	}
 }
 
+func hasPathParameters(member spec.Member) bool {
+	for _, tag := range member.Tags() {
+		if tag.Key == "path" {
+			return true
+		}
+	}
+
+	return false
+}
 func schemaOfField(member spec.Member) swaggerSchemaObject {
 	ret := swaggerSchemaObject{}
 
